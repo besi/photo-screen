7edd3e5d3b059fa4dfbf0193cc8438da70efeb10
Beat Besmer <beat.besmer@gmail.com>
2024-02-03 14:23:05 +0100

    Consolidate the readme

--- Readme.md
+++ Readme.md
@@ -1,15 +1,3 @@
-Installation
-------------
-
-    python3 -m pip install click pyicloud pytz
-
-Install the startup service
-
-    sudo mv photo-screen.service /etc/systemd/system/
-    sudo systemctl daemon-reload
-    sudo systemctl enable photo-screen
-    sudo systemctl start photo-screen
-
 Download images
 ---------------
 
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,20 @@
 mkdir /home/pi/photo-screen/photos
 mkdir /home/pi/photo-screen/photos_old
+
+# Image downloader
+python3 -m pip install click pyicloud pytz
+
+
+# Inky
+sudo apt-get install python3-pip git imagemagick libopenjp2-7-dev jq -y 
+sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED
+pip3 install inky[rpi,example-depends]
+curl https://get.pimoroni.com/inky | bash
+
+cd
+git clone https://github.com/pimoroni/inky
+
+
 # Software update
 sudo cp etc/software-update.service /etc/systemd/system/
 sudo systemctl daemon-reload
